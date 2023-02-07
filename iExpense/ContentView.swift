4ad7020a96ce572bd5c68ac6c208b3edec2bb333
Shahid Arshad <shahid.arshad@devsinc.com>
2023-02-07 16:23:13 +0500

    Create a form to add an item

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAddExpense = false
     @StateObject var expenses = Expenses()
     
     var body: some View {
@@ -21,11 +22,13 @@ struct ContentView: View {
             .navigationTitle("iExpense")
             .toolbar {
                 Button {
-                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
-                    expenses.items.append(expense)
+                    showingAddExpense = true
                 } label: {
                     Image(systemName: "plus")
                 }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
             }
         }
     }
